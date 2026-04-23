1eb15dd4679675b302c8d3f3d650dd7c452d2086
水户洋平 <918475554@qq.com>
2026-04-23 10:32:39 +0800

    feat: 实现全局搜索功能并添加高亮和波纹效果
    新增搜索总线模块(searchBus)用于状态管理
    添加搜索数据模块(searchData)包含可搜索内容
    在导航栏和网络组件中集成搜索功能
    实现关键词匹配、结果高亮和点击波纹动画效果

---
+++ src/utils/searchBus.js
@@ -0,0 +1,59 @@
+import Vue from 'vue'
+
+export const SearchBus = new Vue()
+
+export const SearchState = {
+    searchKeyword: '',
+    matchedItems: [],
+    activeHighlightId: null,
+    rippleEffectId: null,
+    
+    setSearchKeyword(keyword) {
+        this.searchKeyword = keyword
+        SearchBus.$emit('search:keywordChanged', keyword)
+    },
+    
+    setMatchedItems(items) {
+        this.matchedItems = items
+        SearchBus.$emit('search:matchedItemsChanged', items)
+        
+        if (items.length > 0) {
+            this.setActiveHighlightId(items[0].id)
+        } else {
+            this.clearAllHighlights()
+        }
+    },
+    
+    setActiveHighlightId(id) {
+        this.activeHighlightId = id
+        SearchBus.$emit('search:activeHighlightChanged', id)
+    },
+    
+    triggerRippleEffect(id) {
+        this.rippleEffectId = id
+        SearchBus.$emit('search:rippleTriggered', id)
+        
+        setTimeout(() => {
+            this.rippleEffectId = null
+            SearchBus.$emit('search:rippleEnded', id)
+        }, 600)
+    },
+    
+    clearAllHighlights() {
+        this.activeHighlightId = null
+        this.matchedItems = []
+        this.searchKeyword = ''
+        SearchBus.$emit('search:clearAll')
+    },
+    
+    navigateToMatchedItem(item) {
+        if (item && item.hasRoute && item.route) {
+            this.triggerRippleEffect(item.id)
+            SearchBus.$emit('search:navigate', item)
+            return item.route
+        }
+        return null
+    }
+}
+
+export default SearchBus
